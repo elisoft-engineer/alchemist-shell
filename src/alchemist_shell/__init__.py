dc68c6f5b263ddaf2a79c34d721b4b038aea0a6b
Elkana Maina <elisoft.engineer@gmail.com>
2026-07-13 22:45:20 +0300

    fix: automatically get version

--- src/alchemist_shell/__init__.py
+++ src/alchemist_shell/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.1.3"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("alchemist-shell")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
